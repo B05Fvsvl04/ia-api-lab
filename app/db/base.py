efc4b09e2fc471e7e69d7b07cefc85394d8bc87d
Ricardo Gomez <ricardogomezp@yahoo.com>
2026-02-17 21:40:54 -0500

    Add fix

--- app/db/base.py
+++ app/db/base.py
@@ -2,3 +2,7 @@ from sqlalchemy.orm import DeclarativeBase
 
 class Base(DeclarativeBase):
     pass
+
+from app.db.models.user import User
+from app.db.models.refresh_token import RefreshToken
+from app.db.models.login_attempt import LoginAttempt
